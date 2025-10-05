4df874e7d4a1c0bd54d274710f965d66fe97f768
Jorge <110218372+MrJorge898@users.noreply.github.com>
2025-10-05 21:06:02 +0000

    fixing icon issues

--- main.lua
+++ main.lua
@@ -1,14 +1,14 @@
 function love.load()
     timer = 1.5
     
-    quandale = love.audio.newSource("year.mp3", "stream")
-    icon = love.image.newImageData("quandale.ico")
+    local quandale = love.audio.newSource("year.mp3", "stream")
+    local icon = love.image.newImageData("quandale.ico")
 
     quandale:play()
 
     love.window.setTitle("It's been a year daddy")
     love.window.setMode(800, 600, {resizable=false, vsync=1, minwidth=400, minheight=300})
-    icon = love.window.setIcon(icon)
+    local ico = love.window.setIcon(icon)
 end
 
 function love.update(dt)
