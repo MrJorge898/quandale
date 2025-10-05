42ec38eebdb224c95d653938d4db75406753071d
Jorge <110218372+MrJorge898@users.noreply.github.com>
2025-10-05 19:12:26 +0000

    First test

---
+++ quandale/main.lua
@@ -0,0 +1,22 @@
+function love.load()
+    timer = 0
+    
+    quandale = love.audio.newSource("year.mp3", "stream")
+    
+    quandale:play()
+end
+
+function love.update(dt)
+    if timer > 0 then
+        timer = timer - dt
+    end
+
+    if timer < 0 then
+        timer = 1
+        love.event.quit()
+    end
+end
+
+function.love.draw()
+    love.graphics.setBackground(0, 0, 0)
+end
