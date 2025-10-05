7fe127346de1eec35dbc70ec0f8b5ee01507d23c
Jorge <110218372+MrJorge898@users.noreply.github.com>
2025-10-05 21:14:50 +0000

    my HEADDDDD

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ function love.load()
 
     love.window.setTitle("It's been a year daddy")
     love.window.setMode(800, 600, {resizable=false, vsync=1, minwidth=400, minheight=300})
-    local ico = love.window.setIcon(icon)
+    success = love.window.setIcon(icon)
 end
 
 function love.update(dt)
